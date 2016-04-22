a376abcb92359bb49d25346788136df956e478b7
Cory Rylan <splintercode.cb@gmail.com>
2016-04-21 21:47:34 -0500

    Test using firefox for travis

--- karma.conf.js
+++ karma.conf.js
@@ -7,6 +7,7 @@ module.exports = function(config) {
 
         files: [
             // paths loaded by Karma
+            {pattern: 'node_modules/es6-shim/es6-shim.js', included:true, watched: true},
             {pattern: 'node_modules/angular2/bundles/angular2-polyfills.js', included: true, watched: true},
             {pattern: 'node_modules/systemjs/dist/system.src.js', included: true, watched: true},
             {pattern: 'node_modules/rxjs/bundles/Rx.js', included: true, watched: true},
