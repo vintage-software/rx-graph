647867b7bda57c19be59396f745f7880636c7fb1
kevinphelps <KevinPhelps11@gmail.com>
2017-01-10 19:22:32 -0600

    refactor: use export..from statements

--- index.ts
+++ index.ts
@@ -1,23 +1,6 @@
-import { BaseGraphService } from './vstack-graph/graph/base-graph.service';
-import { ServiceConfig, Relation } from './vstack-graph/graph/graph-utilities';
-import { VSCollectionService, CollectionService } from './vstack-graph/services/remote.service';
-import { LocalCollectionService } from './vstack-graph/services/local.service';
-import { AngularHttpMapper } from './vstack-graph/services/angular-http';
-import { CollectionItem } from './vstack-graph/utilities';
-
-export {
-  // Services
-  LocalCollectionService,
-  CollectionService,
-  VSCollectionService,
-  BaseGraphService,
-
-  // Helpers
-  CollectionItem, ServiceConfig, Relation,
-
-  // Mappers
-  AngularHttpMapper
-  // LocalStorageMapper
-  // FirebaseMapper
-  // InMemoryMapper
-};
+export { BaseGraphService } from './vstack-graph/graph/base-graph.service';
+export { ServiceConfig, Relation } from './vstack-graph/graph/graph-utilities';
+export { VSCollectionService, CollectionService } from './vstack-graph/services/remote.service';
+export { LocalCollectionService } from './vstack-graph/services/local.service';
+export { AngularHttpMapper } from './vstack-graph/services/angular-http';
+export { CollectionItem } from './vstack-graph/utilities';
