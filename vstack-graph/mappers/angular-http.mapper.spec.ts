f3bad8a23a94f897d1a1ef3fdff1a41fe25a94ce
Kevin Phelps <kphelps@vintagesoftware.com>
2017-03-20 15:55:38 -0500

    test: fix test build errors

--- vstack-graph/mappers/angular-http.mapper.spec.ts
+++ vstack-graph/mappers/angular-http.mapper.spec.ts
@@ -4,12 +4,14 @@ import 'rxjs/add/observable/of';
 import { AngularHttp } from './angular-http';
 import { AngularHttpMapper } from './angular-http.mapper';
 
+const items = [{ id: 1 }];
+
 let mockAngularHttp: AngularHttp = {
-  get: () => Observable.of({ json: () => true }),
-  put: () => Observable.of({ json: () => true }),
-  post: () => Observable.of({ json: () => true }),
-  patch: () => Observable.of({ json: () => true }),
-  delete: () => Observable.of({ json: () => true })
+  get: (url: string) => Observable.of({ json: () => url.endsWith('/1?') ? items[0] : items }),
+  put: () => Observable.of({ json: () => items }),
+  post: () => Observable.of({ json: () => items }),
+  patch: () => Observable.of({ json: () => items }),
+  delete: () => Observable.of({ json: () => items })
 };
 
 describe('Angular Http Adapter Specs', () => {
@@ -21,13 +23,13 @@ describe('Angular Http Adapter Specs', () => {
 
   it('should have a create method', () => {
     angularHttpMapper.create([]).subscribe(val => {
-      expect(val).toBe(true);
+      expect(val).toBe(items);
     });
   });
 
   it('should have a update method', () => {
     angularHttpMapper.update([]).subscribe(val => {
-      expect(val).toBe(true);
+      expect(val).toBe(items);
     });
   });
 
@@ -39,13 +41,13 @@ describe('Angular Http Adapter Specs', () => {
 
   it('should have a load method', () => {
     angularHttpMapper.load('1').subscribe(val => {
-      expect(val).toBe(true);
+      expect(val).toBe(items[0]);
     });
   });
 
   it('should have a loadMany method', () => {
     angularHttpMapper.loadMany().subscribe(val => {
-      expect(val).toBe(true);
+      expect(val).toBe(items);
     });
   });
 });
