1cb95bbc6d29c40ce8e0086702b35da37aed1661
Kevin Phelps <kphelps@vintagesoftware.com>
2017-03-20 15:57:19 -0500

    test: expect correct mapper method to be called

--- vstack-graph/mappers/angular-http.mapper.spec.ts
+++ vstack-graph/mappers/angular-http.mapper.spec.ts
@@ -4,14 +4,18 @@ import 'rxjs/add/observable/of';
 import { AngularHttp } from './angular-http';
 import { AngularHttpMapper } from './angular-http.mapper';
 
-const items = [{ id: 1 }];
+let getItems = [{ id: 1 }];
+let putItems = [{ id: 2 }];
+let postItems = [{ id: 3 }];
+let patchItems = [{ id: 4 }];
+let deleteItems = [{ id: 5 }];
 
 let mockAngularHttp: AngularHttp = {
-  get: (url: string) => Observable.of({ json: () => url.endsWith('/1?') ? items[0] : items }),
-  put: () => Observable.of({ json: () => items }),
-  post: () => Observable.of({ json: () => items }),
-  patch: () => Observable.of({ json: () => items }),
-  delete: () => Observable.of({ json: () => items })
+  get: (url: string) => Observable.of({ json: () => url.endsWith('/1?') ? getItems[0] : getItems, status: 200 }),
+  put: () => Observable.of({ json: () => putItems, status: 200 }),
+  post: () => Observable.of({ json: () => postItems, status: 201 }),
+  patch: () => Observable.of({ json: () => patchItems, status: 200 }),
+  delete: () => Observable.of({ json: () => deleteItems, status: 204 })
 };
 
 describe('Angular Http Adapter Specs', () => {
@@ -23,31 +27,37 @@ describe('Angular Http Adapter Specs', () => {
 
   it('should have a create method', () => {
     angularHttpMapper.create([]).subscribe(val => {
-      expect(val).toBe(items);
+      expect(val).toBe(postItems);
     });
   });
 
   it('should have a update method', () => {
     angularHttpMapper.update([]).subscribe(val => {
-      expect(val).toBe(items);
+      expect(val).toBe(putItems);
+    });
+  });
+
+  it('should have a patch method', () => {
+    angularHttpMapper.patch([]).subscribe(val => {
+      expect(val).toBe(patchItems);
     });
   });
 
   it('should have a delete method', () => {
     angularHttpMapper.delete([]).subscribe(val => {
-      expect(val).toBe(undefined);
+      expect(val).toBe(204);
     });
   });
 
   it('should have a load method', () => {
     angularHttpMapper.load('1').subscribe(val => {
-      expect(val).toBe(items[0]);
+      expect(val).toBe(getItems[0]);
     });
   });
 
   it('should have a loadMany method', () => {
     angularHttpMapper.loadMany().subscribe(val => {
-      expect(val).toBe(items);
+      expect(val).toBe(getItems);
     });
   });
 });
